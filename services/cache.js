14df14b57d42f9a09f5703c848f1084e27d4b917
Jose Mendoza <josemendoza@Joses-MacBook-Pro.local>
2018-04-17 12:05:31 -0600

    toggable cache

--- services/cache.js
+++ services/cache.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
 const { exec } = mongoose.Query.prototype;
+// Setup REDIS + promisify get function
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 
 // REMINDER https://bit.ly/2qEmUkN
 // Arrow function vs function declaration / expressions:
@@ -9,8 +15,38 @@ const { exec } = mongoose.Query.prototype;
 // are not equivalent and cannot be replaced blindly.
 // If the function you want to replace does not use this,
 // arguments and is not called with new, then yes.
-mongoose.Query.prototype.exec = async function execOverride(...args) {
-  console.log('EXECUTING QUERY');
+
+mongoose.Query.prototype.cache = function cache() {
+  // this equals query instance
+  this.useCache = true;
+  // return makes it chainable
+  return this;
+};
+
+mongoose.Query.prototype.exec = async function execAndCache(...args) {
+  if (!this.useCache) {
+    return exec.apply(this, args);
+  }
+  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name
+  }));
+  // Search cache.
+  const cachedValue = await client.get(key);
+  if (cachedValue) {
+    // Function expects to return a Mongoose object.
+    // Mongoose model with properties like get, get, etc.
+    const doc = JSON.parse(cachedValue);
+
+    /* eslint-disable */
+    const cachedDocument = Array.isArray(doc) 
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
+    /* eslint-enable */
+    return cachedDocument;
+  }
+
+  // If not there execute query and cache result.
   const result = await exec.apply(this, args);
+  client.set(key, JSON.stringify(result));
   return result;
 };
